cae9f7a97c6532c78c7bbbe74f341e143031b9eb
Krystal Williams <asterika@Krystals-MacBook-Pro.local>
2018-11-27 23:25:00 -0800

    Testing /shops path on Heroku

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::get('/locate', 'PagesController@locate');
 //===========================================
 //          ROUTES FOR CRUD PAGES
 //===========================================
-Route::resource('shops', 'ShopsController');
+Route::resource('/shops', 'ShopsController');
 
 //===========================================
 //             ROUTES FOR AUTH
