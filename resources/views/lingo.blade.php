ac324dd7984a73899c2b3a57259418b4544f0db5
Krystal Williams <asterika@Krystals-MacBook-Pro.local>
2018-11-29 23:54:06 -0800

    Added emphasis to lingo page

--- resources/views/lingo.blade.php
+++ resources/views/lingo.blade.php
@@ -16,24 +16,25 @@
         <img src="/images/gibraltar_cortado.jpg" style="height:50px; width:50px; border-radius:40px;">&emsp;&emsp;&emsp;
 
           <ul>
-            <li><em>Espresso:</em> The base for all espresso drinks. A strong shot of coffee: 2 shots of espresso = approximately 2 ounces. Shots are created by forcing finely ground beans into water at high pressure.</li>
-            <li><em>Macchiato:</em> 2 shots espresso + 1 oz. lightly steamed milk (120&#8457;)</li>
-            <li><em>Gibraltar/Cortado:</em> Espresso with equal parts lightly steamed milk | Standard: 2 ounces espresso + 2 ounces lightly steamed milk<br > (110&#8457;)</li><br >
+            <li><strong><em>Espresso:</strong></em> The base for all espresso drinks. A strong shot of coffee: 2 shots of espresso = approximately 2 ounces. Shots are created by forcing finely ground beans into water at high pressure.</li>
+            <li><strong><em>Macchiato:</em></strong> 2 shots espresso + 1 oz. lightly steamed milk (120&#8457;)</li>
+            <li><strong><em>Macchiato:</em></strong> 2 shots espresso + 1 oz. lightly steamed milk (120&#8457;)</li>
+            <li><strong><em>Gibraltar/Cortado:</em></strong> Espresso with equal parts lightly steamed milk | Standard: 2 ounces espresso + 2 ounces lightly steamed milk<br > (110&#8457;)</li><br >
 
             <img src="/images/cappuccino.jpg" style="height:50px; width:50px; border-radius:40px;">&emsp;&emsp;&emsp;
             <img src="/images/flat_white.jpg" style="height:50px; width:50px; border-radius:40px;">&emsp;&emsp;&emsp;
             <img src="/images/latte.jpg" style="height:50px; width:50px; border-radius:40px;">
-            <li><em>Cappuccino:</em> 2 shots espresso + 3-6 ounces steamed milk with microfoam<br >(130&#8457;)</li>
-            <li><em>Flat White:</em> 2 shots espresso + 6 ounces steamed milk, very little microfoam</li>
-            <li><em>Latte:</em> 2 shots espresso + 6-10 ounces steamed milk, light microfoam<br >(145&#8457;)</li><br >
+            <li><strong><em>Cappuccino:</em></strong> 2 shots espresso + 3-6 ounces steamed milk with microfoam<br >(130&#8457;)</li>
+            <li><strong><em>Flat White:</em></strong> 2 shots espresso + 6 ounces steamed milk, very little microfoam</li>
+            <li><strong><em>Latte:</em></strong> 2 shots espresso + 6-10 ounces steamed milk, light microfoam<br >(145&#8457;)</li><br >
 
 
             &emsp;&emsp;<img src="/images/mocha.jpg" style="height:50px; width:50px; border-radius:40px;">&emsp;&emsp;&emsp;
             <img src="/images/cold_brew.jpg" style="height:50px; width:50px; border-radius:40px;">&emsp;&emsp;&emsp;
             <img src="/images/nitro_cold_brew.jpg" style="height:50px; width:50px; border-radius:40px;">&emsp;&emsp;&emsp;
-            <li><em>Mocha:</em> A latte with chocolate syrup, like a hot chocolate with espresso</li>
-            <li><em>Cold Brew:</em> Coarsely ground coffee steeped in cold water for 16-24 hours</li>
-            <li><em>Nitro Cold Brew:</em> Cold brew that has been nitrogen-infused</li>
+            <li><strong><em>Mocha:</em></strong> A latte with chocolate syrup, like a hot chocolate with espresso</li>
+            <li><strong><em>Cold Brew:</em></strong> Coarsely ground coffee steeped in cold water for 16-24 hours</li>
+            <li><strong><em>Nitro Cold Brew:</em></strong> Cold brew that has been nitrogen-infused</li>
           </ul>
       </div>
 
