0fb1c09d6a8ceff5803b77721e94f3d865e9f1a0
Krystal Williams <asterika@Krystals-MacBook-Pro.local>
2018-11-29 23:04:45 -0800

    Added iframe to locate page - testing

--- resources/views/locate.blade.php
+++ resources/views/locate.blade.php
@@ -10,9 +10,10 @@
     <div class="form-container">
       <img src="/images/coffee_shop_sign.jpg" class="sidebar" alt="Coffee Shop Sign | Photo by Nathan Dumlao on Unsplash">
         <div class="form-box"><br ><br >
-          <h1><strong>Visit <a href="https://thirdwavenearme.com/"><em>Third Wave Near Me</em></a></strong></h1><br ><br >
+          <iframe src="https://thirdwavenearme.com/"></iframe>
+          <!-- <h1><strong>Visit <a href="https://thirdwavenearme.com/"><em>Third Wave Near Me</em></a></strong></h1><br ><br >
               <a href="https://thirdwavenearme.com/" target="_blank"><img src="/images/third_wave_near_me.jpg" style="height:35%; width:70%;"></a><br ><br ><br >
-                <p>This app uses Google map overlays to find speciality coffee shops in your area</p>
+                <p>This app uses Google map overlays to find speciality coffee shops in your area</p> -->
         </div>
     </div>
   </div>
